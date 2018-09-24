4e582be97a2d5998e7636357d18ccb9f171bdf3a
hadarth <hadar.schindler@gmail.com>
2018-09-24 22:03:26 +0300

    Added two buttons for start and stop the spin

--- src/Sploader.js
+++ src/Sploader.js
@@ -2,14 +2,27 @@ import React, { Component } from 'react';
 
 export default class Sploader extends Component {
 
-  renderSploader(percent){
+  constructor(props, context) {
+    super(props);
+    this.SploaderSvg = React.createRef();
+  }
+
+  renderSploader(percent) {
     const strokeDashoffset = 100 - percent
     return (
-      <svg key="svg" className="spinit" xmlns='http://www.w3.org/2000/svg' viewBox='0 0 200 200'>
+      <svg ref={this.SploaderSvg} className='spinit' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 200 200'>
         <circle className='base' cx='100' cy='100' r='90' />
         <circle style={{ strokeDashoffset }} className='blue' cx='100' cy='100' r='90' pathLength="100"/>
       </svg>
     )
+  }
+
+  start() {
+    this.SploaderSvg.current.style.animationPlayState = ""
+  }
+
+  stop() {
+    this.SploaderSvg.current.style.animationPlayState = "paused"
   }
 
   render() {
@@ -25,6 +38,8 @@ export default class Sploader extends Component {
           </div>
         </div>
         <div className="buttons">
+          <div className="start" onClick={() => this.start()}>start spinning</div>
+          <div className="stop" onClick={() => this.stop()}>stop spinning</div>
         </div>
       </div>
     );
