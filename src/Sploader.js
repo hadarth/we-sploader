d33cb2a64781c3470fb8483da7c016e5b7f242e8
hadarth <hadar.schindler@gmail.com>
2018-09-24 18:18:10 +0300

    Added Sploader component and css files

---
+++ src/Sploader.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+export default class Sploader extends Component {
+
+  renderSploader(percent){
+    const strokeDashoffset = 100 - percent
+    return (
+      <svg key="svg" className="svg_container" xmlns='http://www.w3.org/2000/svg' viewBox='0 0 200 200'>
+        <circle className='base' cx='100' cy='100' r='90' />
+        <circle style={{ strokeDashoffset }} className='blue' cx='100' cy='100' r='90' pathLength="100"/>
+      </svg>
+    )
+  }
+
+  render() {
+    return (
+      <div className="container">
+      { this.renderSploader(this.props.percent) }
+      </div>
+    );
+  }
+}
