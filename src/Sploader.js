7d1cac2288862a905e2202cc9f7134116e2f1f23
hadarth <hadar.schindler@gmail.com>
2018-09-24 20:16:04 +0300

    Changed the div structure and corresponding css to centre the percentage in the circle Added button container placeholder

--- src/Sploader.js
+++ src/Sploader.js
@@ -5,7 +5,7 @@ export default class Sploader extends Component {
   renderSploader(percent){
     const strokeDashoffset = 100 - percent
     return (
-      <svg key="svg" className="svg_container spinit" xmlns='http://www.w3.org/2000/svg' viewBox='0 0 200 200'>
+      <svg key="svg" className="spinit" xmlns='http://www.w3.org/2000/svg' viewBox='0 0 200 200'>
         <circle className='base' cx='100' cy='100' r='90' />
         <circle style={{ strokeDashoffset }} className='blue' cx='100' cy='100' r='90' pathLength="100"/>
       </svg>
@@ -15,8 +15,17 @@ export default class Sploader extends Component {
   render() {
     return (
       <div className="container">
-        { this.renderSploader(this.props.percent) }
-        <div className="loading_value">{ this.props.percent }%</div>
+        <div className="sploader_container">
+          <div className="svg_container">
+            { this.renderSploader(this.props.percent) }
+          </div>
+          <div className="loading_value">
+            { this.props.percent }
+            <span className="percent">%</span>
+          </div>
+        </div>
+        <div className="buttons">
+        </div>
       </div>
     );
   }
