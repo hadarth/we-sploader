accaa38fff85b3c1591a87aa085ac0ede9a519d6
hadarth <hadar.schindler@gmail.com>
2018-09-25 00:47:55 +0300

    handling percentage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import React, { Component } from 'react';
 import Sploader from './Sploader'
-import './Sploader.css';
+import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {percent: '', value: ''};
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  onClick (value) {
+    this.setState({percent: value})
+  }
+
   render() {
     return (
-      <Sploader percent={13}/>
+      <div className='ss'>
+        <label className='padded'>
+          Enter loading:
+          <input type="text" value={this.state.value} onChange={this.handleChange} />
+          <button onClick={() => this.onClick(this.state.value)}>Submit</button>
+        </label>
+        <Sploader percent={this.state.percent || 13}/>
+      </div>
     );
   }
 }
